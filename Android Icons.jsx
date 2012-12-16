e637542067ef2490a7a715f02f36485c51a4da48
Tim Roes <mail@timroes.de>
2012-12-16 13:03:10 +0100

    Android Icon Tool

---
+++ Android Icons.jsx
@@ -0,0 +1,146 @@
+// Android Icons 
+// Author: Tim Roes <mail@timroes.de>
+
+#target photoshop
+
+app.bringToFront();
+
+var ICON_DIR = "Icons/";
+var HOLO_LIGHT = "holo_light/";
+var HOLO_DARK = "holo_dark/";
+
+var holoLight;
+var holoDark;
+
+var search;
+
+var iconList;
+var allIcons;
+
+/**
+ * Change color of the list and reload icons, when holo theme changes.
+ */
+var holoChanged = function(ev) {
+	var bgColor = (holoDark.value == true) ? [0.13, 0.13, 0.13] : [0.95, 0.95, 0.95];
+	var color = (holoDark.value == true) ? [1, 1, 1] : [0, 0, 0];
+	iconList.graphics.backgroundColor = iconList.graphics.newBrush(iconList.graphics.BrushType.SOLID_COLOR, bgColor);
+	iconList.graphics.foregroundColor = iconList.graphics.newPen(iconList.graphics.PenType.SOLID_COLOR, color, iconList.graphics.foregroundColor.lineWidth);
+	loadIcons();
+};
+
+/**
+ * Build the UI.
+ */
+function buildIconUI() {
+	var win = new Window('dialog', 'Android Icons');
+	win.alignChildren = "left";
+	win.addEventListener("keydown", function(ev) {
+		search.active = true;
+	});
+
+	var searchGroup = win.add("group");
+	var searchLabel = searchGroup.add("statictext");
+	searchLabel.text = "Search:";
+	search = searchGroup.add("edittext");
+	search.characters = 30;
+	search.active = true;
+	search.onChanging = function(ev) {
+		updateList();
+	};
+
+	var holoGroup = win.add("group");
+	holoGroup.orientation = "row";
+	holoLight = holoGroup.add("radiobutton", undefined, "Holo &Light");
+	holoDark = holoGroup.add("radiobutton", undefined, "Holo &Dark");
+	holoLight.value = true;
+	holoLight.onClick = holoChanged;
+	holoDark.onClick = holoChanged;
+
+	iconList = win.add("listbox", [0, 0, 300, 200]);
+	iconList.onClick = function(ev) {
+		loadFileToNewLayer(iconList.selection.file_object);
+		win.close();
+	};
+
+	holoChanged();
+	win.show();
+}
+
+function getHolo() {
+	return (holoDark.value == true) ? HOLO_DARK : HOLO_LIGHT;
+}
+
+/**
+ * Load all icons from the icon path and the selected Holo theme.
+ */
+function loadIcons() {
+
+	// Find all icons
+	var iconDir = Folder.commonFiles;
+	iconDir.changePath("AndroidTools");
+	iconDir.changePath(ICON_DIR);
+	iconDir.changePath(getHolo());
+
+	var icons = iconDir.getFiles("*.png");
+	allIcons = new Array();
+	
+	for(var icon in icons) {
+		allIcons.push(icons[icon]);
+	}
+
+	allIcons.sort();
+	updateList();
+
+}
+
+/**
+ * Update the list of icons, taking the current search filter into respect.
+ */
+function updateList() {
+
+	var filter = search.text;
+
+	iconList.removeAll();
+
+	for(var i in allIcons) {
+		if(!filter || allIcons[i].displayName.indexOf(filter) !== -1) {
+			var item = iconList.add("item", allIcons[i].displayName.slice(0, -4));
+			item.file_object = allIcons[i];
+			item.image = allIcons[i];
+		}
+	}
+
+}
+
+/**
+ * Load a file into a new layer.
+ */
+function loadFileToNewLayer(file) {
+
+	// Place object as layer
+	// I have no idea, what all that does... But I assume there must be a more elegant way.
+	var desc = new ActionDescriptor();
+	desc.putPath( charIDToTypeID('null'), file );
+	desc.putEnumerated( charIDToTypeID('FTcs'), charIDToTypeID('QCSt'), charIDToTypeID('Qcsa') );
+	var offsetDesc = new ActionDescriptor();
+	offsetDesc.putUnitDouble( charIDToTypeID('Hrzn'), charIDToTypeID('#Pxl'), 0.000000 );
+	offsetDesc.putUnitDouble( charIDToTypeID('Vrtc'), charIDToTypeID('#Pxl'), 0.000000 );
+	desc.putObject( charIDToTypeID('Ofst'), charIDToTypeID('Ofst'), offsetDesc );
+	executeAction( charIDToTypeID('Plc '), desc, DialogModes.NO );
+
+	// The following line is nice. I want the rest above to be looking the same.
+	app.activeDocument.activeLayer.rasterize(RasterizeType.ENTIRELAYER);
+
+}
+
+if(!documents.length) {
+	alert('There are no documents open.', 'No Documents Open', true);
+} else if(parseInt(version, 10) < 10) {
+	alert('This script requires at least Photoshop CS3.', 'Wrong Photoshop Version', true);
+} else {
+	try {
+		buildIconUI();
+	} catch(e) {
+		alert(e, 'Android Icons Error', true);
+	}
+}
---
+++ README.md
@@ -0,0 +1,63 @@
+Android Photoshop Tools
+=======================
+
+The Android Photoshop Tools are some small tools, meant to simplify the
+design process of Android applications in Photoshop. The different tools
+are described further down.
+
+
+Installation
+------------
+
+To install the tools do the following steps:
+
+1. Download all files (use the ZIP button above).
+2. Move all .jsx files to the Photoshop Script folder
+   - Windows: C:\Program Files\Adobe\Adobe Photoshop CSx\Presets\Scripts\
+   - Mac: /Applications/Photoshop CSx/Presets/Scripts/
+3. Move the `AndroidTool` directory to its place:
+   - Windows: C:\Program Files\Common Files\
+   - Mac: /Library/Application Support/
+4. If you now start Photoshop, you can start the tool you want 
+   via File->Scripts-><Tool Name>. You can set shortcuts for the script in
+   the shortcut menu.
+
+
+License
+-------
+
+This tools are licensed under MIT license and are thus free to use and modify
+an distribute however you wish.
+
+The Android Icons and Sources belong to Google and were downloaded from
+http://developer.android.com/design/downloads/index.html
+Refere to that page for usage information.
+
+
+Tested
+------
+
+All tools have been tested with Adobe Photoshop CS5, but hopefully will also work
+with other versions (from CS3 and above).
+
+If you find errorsm have problems using the tools or suggestions on how to improve them,
+drop me a notice. You can reach me either via the Github Issues for this project, my 
+mail address (mail@timroes.de) or most of the time in the #android-dev channel on irc.freenode.net.
+
+
+Android Icons
+-------------
+
+This tool allow easy access to the Android Action Bar Icons.
+
+You can search for icon names and chose a Holo theme (either by selecting
+in the UI or by pressing Alt + D (Holo Dark) or Alt + L (Holo Light)).
+If you click on an icon in the list, it will be placed in a new layer in
+your currently active document.
+
+All Icons are mdpi resolution, since also the Android sources are in mdpi 
+resolution.
+
+You can use this tool also for additional icons. Just place them  (must be png files)
+into `AndroidTools/Icons/holo_dark` or `AndroidTools/Icons/holo_light` (see
+Installation above for the location of the `AndroidTools` folder).
